17a76092e8a92629c238dcf66921a3d7bd2c8d4d
justRunnz <ruigaspar03@hotmail.com>
2022-01-25 10:10:40 +0100

    Change mysqli to PDO
    💙 Add insert into CompteBancaire

--- vendor/insertData.php
+++ vendor/insertData.php
@@ -1,30 +1,17 @@
 <?php
+  require_once './connectMysql.php';
   function insertData(){
     if(isset($_POST["submitForm"])){
-      $conn = mysqli_connect("mysql-rttaphp.alwaysdata.net", "rttaphp_formulaire", "rttaphp_rui", "justrunnz");
 
-      // $db = connectMysql();
+      $nom_compte = $_POST['nom_compte'];
+      $type_compte = $_POST['type_compte'];
+      $provision_compte = $_POST['provision_compte'];
+      $devise_compte = $_POST['devise_compte'];
 
-      $nom_compte = $_REQUEST['nom_compte'];
-      $type_compte = $_REQUEST['type_compte'];
-      $provision_compte = $_REQUEST['provision_compte'];
-      $devise_compte = $_REQUEST['devise_compte'];
-
-
-      $req = "INSERT INTO CompteBancaire VALUES ('$nom_compte','$type_compte','$provision_compte','$devise_compte')";
-      // $req->execute(array("nom" => $_POST['nom'], "type_compte" => $_POST['type_compte'], "provision" => $_POST['provision'], "devise" => $_POST['devise']));
-    
-      if(mysqli_query($conn, $req)){
-        echo "<h3>data stored in a database successfully." 
-            . " Please browse your localhost php my admin" 
-            . " to view the updated data</h3>"; 
-
-        echo nl2br("\n$nom_compte\n $type_compte\n "
-            . "$provision_compte\n $devise_compte");
-      } else{
-          echo "ERROR: Hush! Sorry $req. " 
-              . mysqli_error($conn);
-      }
+      $db = connectMysql();
+      $req = $db->prepare('INSERT INTO CompteBancaire ( IdUtilisateur, Nom_Compte, Type_Compte, Provision_Compte, Devise_Compte) VALUES ( :idU, :NC, :TC, :PC, :DC) ');
+      $req->execute( array( 'idU' => 3, 'NC' => $nom_compte, 'TC' => $type_compte, 'PC' => $provision_compte, 'DC' => $devise_compte ) );
+      echo "$req";
     }
   }
   insertData();
