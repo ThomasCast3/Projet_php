ae482a1d2995ac704be6def9d66c052d478d815f
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-01-25 16:05:46 +0100

    add an alert and a return page for adding count

--- vendor/insertData.php
+++ vendor/insertData.php
@@ -20,8 +20,11 @@
         $req = $db->prepare('INSERT INTO CompteBancaire ( IdUtilisateur, Nom_Compte, Type_Compte, Provision_Compte, Devise_Compte) VALUES ( :idU, :NC, :TC, :PC, :DC) ');
         $req->execute( array( 'idU' => 3, 'NC' => $nom_compte, 'TC' => $type_compte, 'PC' => $provision_compte, 'DC' => $devise_compte ) );
       }else {
-        echo "<script>alert(\"Nombre de compte dépassé\")</script>";
-        // header("index.php");
+        echo "<script>
+                alert(\"Nombre de compte dépassé\");
+                window.location.href = '../index.php';
+              </script>";
+        // header("location:../index.php");
       }
     }
   }
