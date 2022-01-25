7e926b9edce769a70fc2064ac328db97bb0f3239
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-01-25 12:12:03 +0100

    Add function to security html

--- vendor/insertData.php
+++ vendor/insertData.php
@@ -13,18 +13,25 @@
 
       $count = $db->query('SELECT COUNT(*) FROM CompteBancaire WHERE IdUtilisateur = 3 ')->fetchColumn();
       // $count->execute( array( 'idU' => 3 ) );
-      // var_dump($count);
+
+      // $delete = $db->query('DELETE FROM CompteBancaire WHERE IdUtilisateur = 3 ')->fetchColumn();
 
       if ($count < 11) {
         $req = $db->prepare('INSERT INTO CompteBancaire ( IdUtilisateur, Nom_Compte, Type_Compte, Provision_Compte, Devise_Compte) VALUES ( :idU, :NC, :TC, :PC, :DC) ');
         $req->execute( array( 'idU' => 3, 'NC' => $nom_compte, 'TC' => $type_compte, 'PC' => $provision_compte, 'DC' => $devise_compte ) );
       }else {
-        echo "Nombre de compte dépassé";
+        echo "<script>alert(\"Nombre de compte dépassé\")</script>";
+        header("index.php");
       }
       
       
     }
   }
+
+  function h($text) { 
+    return htmlspecialchars($text);
+  }
+
   insertData();
 
 ?>
