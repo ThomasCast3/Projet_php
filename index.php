c290a733cd6ef3a73c9cc1939f1b99f0fc8f8baf
Theo Landemaine <theo.landemaine@hotmail.fr>
2022-01-25 15:24:51 +0100

    delete test connecting js file

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <meta charset="utf-8">
     <title>Little Accountant</title>
     <link rel="stylesheet" href="./style/style.css">
-    <script src="easterEgg.js"></script>
   </head>
 
   <body>
