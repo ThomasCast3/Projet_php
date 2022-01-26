0894ac1f63a9fd36e599e55a2e1f487e13f4712e
justRunnz <ruigaspar03@hotmail.com>
2022-01-26 16:12:22 +0100

    Add option delete account

--- vendor/deleteAccountBank.php
+++ vendor/deleteAccountBank.php
@@ -1,3 +1,25 @@
 <?php
+require 'connectMysql.php';
 
-echo 'test';
+session_start();
+$db = connectMysql();
+
+if(!isset($_SESSION['user_login']))	//check unauthorize user not access in "welcome.php" page
+{
+    header("location: deleteAccountBank.php");
+}
+
+$id = $_SESSION['user_login'];
+
+
+$sqlAccount = 'DELETE FROM CompteBancaire WHERE IdUtilisateur = :id';
+// prepare the statement for execution
+$statement = $db->prepare($sqlAccount);
+$statement->execute(array(':id'=> $id));
+
+// execute the statement
+if ($statement->execute()) {
+	echo 'publisher id ' . $id . ' was deleted successfully.';
+}
+
+//session_destroy();
