aa1d70a32eaa7ca2d2593c6374bcbe7d52e4124c
justRunnz <ruigaspar03@hotmail.com>
2022-01-26 16:12:32 +0100

    Update createAccount.php

--- data/createAccount.php
+++ data/createAccount.php
@@ -2,10 +2,11 @@
 session_start();
 
 require_once "../vendor/connectMysql.php";
+include_once "./createAccountHtml.php";
 
 if(isset($_SESSION["user_login"]))	//check condition user login not direct back to index.php page
 {
-	header("location: welcome.php");
+	header("location: welcomeHtml.php");
 }
 
 
@@ -52,10 +53,10 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
 																(:email,:password,:username)'); 		//sql insert query					
 				
 				if($insert_stmt->execute(array(':email'	=>$email, ':password'=>$new_password, ':username'=>$username))){
-          $_SESSION["user_login"] = $db->lastInsertId();	//session name is "user_login"
-          notifC('Register Successfully...');
-          //header("refresh:2; welcome.php");			//refresh 2 second after redirect to "welcome.php" page
-        }
+					$_SESSION["user_login"] = $db->lastInsertId();	//session name is "user_login"
+					notifC('Register Successfully...');
+					header("refresh:2; welcomeHtml.php");			//refresh 2 second after redirect to "welcome.php" page
+        		}
 			}
 		}
 		catch(PDOException $e)
@@ -65,81 +66,3 @@ if(isset($_REQUEST['btn_register'])) //button name "btn_register"
 	}
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="initial-scale=1.0, maximum-scale=2.0">
-    <link rel="stylesheet" href="../style/styleCreateAccount.css">	
-  </head>
-
-	<body>
-    <header>
-      <div class="header-container">
-        <h2>Create Your Account</h2>
-      </div>
-    </header>
-
-		
-		<div class="container">
-      <?php
-      function notifE($text)
-      {?>
-        <div id="notification_container">
-          <div class="content">
-              <p><?php echo $text ?></p>
-          </div>
-        </div>
-        <?php
-      }
-      function notifC($text)
-      {?>
-        <div id="notification_container">
-            <div class="content_notif_correct">
-              <p><?php echo $text ?></p>
-            </div>
-        </div>
-        <?php
-        }
-      ?> 
-  
-      <form method="post" class="form-horizontal">
-
-        <div class="form-group">
-          <label class="label">Username</label>
-          <input type="text" name="txt_username" class="form-control" placeholder="enter username" />
-				</div>
-          
-        <div class="form-group">
-          <label class="label">Email</label>
-          <input type="text" name="txt_email" class="form-control" placeholder="enter email" />
-        </div>
-          
-        <div class="form-group">
-        <label class="label">Password</label>
-          <input type="password" name="txt_password" class="form-control" placeholder="enter passowrd" />
-        </div>
-          
-        <div class="form-group">
-          <input type="submit"  name="btn_register" class="btn btn-primary " value="Register">
-        </div>
-        
-        <div class="form-group">
-          You have a account register here? <a href="login.php">
-          <p class="text-info">Login Account</p></a>		
-        </div>
-          
-      </form>
-		</div>
-										
-	</body>
-</html>
-
-
-<script>
-  // Add a timer to supress our notifications after 3 seconds
-  setTimeout(function () {
-        document.querySelector('#notification_container .content').remove();
-    }, 3000);
-</script>
