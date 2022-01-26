792df16693eb43819d5dac3ae7b7d01eb8c01343
justRunnz <ruigaspar03@hotmail.com>
2022-01-26 16:12:40 +0100

    Update welcome.php

--- data/welcome.php
+++ data/welcome.php
@@ -1,47 +1,20 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, maximum-scale=2.0">
+<?php            
+    require_once '../vendor/connectMysql.php';
+    include_once './welcomeHtml.php';
 
-    </head>
+    $db = connectMysql();
 
-    <body>
-        <div class="container">
-            <center>
-                <h2>
-                <?php
-                
-                require_once '../vendor/connectMysql.php';
-                $db = connectMysql();
-                
-                session_start();
+    session_start();
 
-                if(!isset($_SESSION['user_login']))	//check unauthorize user not access in "welcome.php" page
-                {
-                    header("location: login.php");
-                }
-                
-                $id = $_SESSION['user_login'];
-                
-                $select_stmt = $db->prepare("SELECT * FROM Utilisateurs WHERE IdUtilisateur=:IdUtilisateur");
-                $select_stmt->execute(array(":IdUtilisateur"=>$id));
+    if(!isset($_SESSION['user_login']))	//check unauthorize user not access in "welcome.php" page
+    {
+        header("location: login.php");
+    }
 
-                $row=$select_stmt->fetch(PDO::FETCH_ASSOC);
-                
-                if(isset($_SESSION['user_login']))
-                {
-                ?>
-                    Welcome,
-                <?php
-                    echo $row['username'];
-                }
-                ?>
-                </h2>
-                    <a href="logout.php">Logout</a>
-            </center>
-        </div>
+    $id = $_SESSION['user_login'];
 
-    </body>
-</html>
+    $select_stmt = $db->prepare("SELECT * FROM Utilisateurs WHERE IdUtilisateur=:IdUtilisateur");
+    $select_stmt->execute(array(":IdUtilisateur"=>$id));
+
+    $row=$select_stmt->fetch(PDO::FETCH_ASSOC);
+?>
