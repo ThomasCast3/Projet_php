41197bd7bb278cffbc59532533b23ab03b73d23c
justRunnz <ruigaspar03@hotmail.com>
2022-01-27 09:59:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <head>
     <meta charset="utf-8">
     <title>Little Accountant</title>
-    <link rel="stylesheet" href="./assets/style/style.css">
+    <link rel="stylesheet" href="../../assets//style/style.css">
     <?php include('./vendor/accountManagement.php') ?>
   </head>
 
@@ -20,7 +20,9 @@
     <p>Select a Bank Account :
       <select id="menuDeroulan" name="type_compte">
         <option value="">-- Bank Account --</option>
-          <?php foreach( ListCompte( 65 ) as $Compte ): ?>   <!--creer une boucle for sur la fonction listCompte pour l'utilisateur 3 -->
+          <?php 
+              $idUser = $_SESSION['user_login'];
+              foreach( ListCompte( $idUser ) as $Compte ): ?>   <!--creer une boucle for sur la fonction listCompte pour l'utilisateur 3 -->
               <option data-full='<?=json_encode($Compte); ?>' value="<?= $Compte['IdCompte']; ?>"><?= $Compte['Nom_Compte']; ?></option>  <!-- creer un option dans select avec l'id du compte et afficher son nom -->
           <?php endforeach; ?>  <!--  fin boucle for -->
       </select>
@@ -32,7 +34,7 @@
           OPTIONS
           <ul class="sub-menu">
             <li>
-              <a href="./addAccountBankHTML.php">
+              <a href="../../addAccountBankHtml.php">
                 ADD
               </a>
             </li>
@@ -44,7 +46,7 @@
                 <input type="hidden" name="IdCompte"/>
                 DELETE
               </a> -->
-              <form id="deleteAccountForm" method="POST" action="./vendor/deleteAccountBank.php">
+              <form id="deleteAccountForm" method="POST" action="../../vendor/deleteAccountBank.php">
                 <input type="hidden" name="IdCompte" />
                 <input type="submit" value="DELETE" />
               </form>
@@ -81,8 +83,9 @@
         <p><input type="submit" name="submitFormEdit" value="OK"></p>
       </form>
     </div>
-  
-    <script src="./assets/js/accountManagement.js"></script>
+    <a href="./vendor/php/logout.php">Connect to access your accounts</a>
+
+    <script src="../../assets/js/accountManagement.js"></script>
 
   </body>
 </html>
